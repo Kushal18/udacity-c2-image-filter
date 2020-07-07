081a052925bb0e479ba412cb8b8c214d19fa4c3c
“Kushal <kjoshi@marketo.com>
2020-07-06 17:30:42 -0700

    filtered image initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Udagram Image Filtering Microservice
+
+Udagram is a simple cloud application developed alongside the Udacity Cloud Engineering Nanodegree. 
+
+This project consists of a end point (filteredimage?image_url). The passed in query parameter downloads the image from a public url, filters the image and returns the result. 
+
+
+
+## Tasks
+
+### Setup Node Environment
+
+You'll need to create a new node server. Open a new terminal within the project directory and run:
+
+1. Initialize a new project: `npm i`
+2. run the development server with `npm run dev`
+
+### Access Points
+ 
+AWS: udagaram-c2-image-filter-kdj-dev.us-west-2.elasticbeanstalk.com
---
+++ src/server.ts
@@ -0,0 +1,49 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // GET /filteredimage?image_url={{URL}}
+  app.get('/filteredimage/', 
+    async ( req, res ) => {
+    let { image_url } = req.query;
+    if( !image_url ) {
+      return res.status(400)
+                .send(`image url is required`);
+    }
+    let filteredImage: string; 
+    try {
+      filteredImage = await filterImageFromURL(image_url);
+    } catch ( error ) {
+      return res.status(415)
+                .send(`Image url not found`);
+    }
+    return res.status(200)
+              .sendFile(filteredImage, () => {
+                deleteLocalFiles([filteredImage])
+              }); 
+  });
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
